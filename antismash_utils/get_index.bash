62f08ed7d9aefb23ea8276388daea6aa466fe73e
DrBoothTJ <booth.thom@gmail.com>
2024-02-13 14:38:46 +0100

    update argument parsing

--- antismash_utils/get_index.bash
+++ antismash_utils/get_index.bash
@@ -3,5 +3,5 @@
 ### Requires: jq (sudo apt install jq)
 ### Arguments: $1 : .json file path
 
-jq -r '.records[].areas[] | [.start, .end, .products] | flatten | @tsv' $1
+jq -r '.records[].areas[] | [.start, .end, .products] | flatten | @tsv' "$@"
 
