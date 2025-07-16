0023360cb6997c148db8944d7c59823dccf5bc7a
drboothtj <booth.thom@gmail.com>
2025-07-16 14:23:47 +0200

    update sed

--- ncbi_utils/download_metagenomes.bash
+++ ncbi_utils/download_metagenomes.bash
@@ -15,6 +15,7 @@ cat metagenomes_tbl.tsv | cut -f6 > ftp_links.txt
 
 # for .gbff you can just substitute
 # e.g.  sed 's/GCA/GCF/g' ftp_addresses.txt > gbff_adresses.txt
+# sed 's/gff/gbff/g' ftp_addresses.txt > gbff_adresses.txt
 
 #set up a shell script for downloading (DO IT IN SCREEN!)
 awk 'BEGIN{FS=OFS="/";filesuffix="genomic.gff.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print "rsync -t -v "ftpdir,file" ./"}' ftp_links.txt | sed 's/https/rsync/g' > download_gff_files.sh
