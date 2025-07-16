467d5ed6e3f11ebcd3c72928b82e344c0b80c279
drboothtj <booth.thom@gmail.com>
2025-07-16 14:04:14 +0200

    add sed

--- ncbi_utils/download_metagenomes.bash
+++ ncbi_utils/download_metagenomes.bash
@@ -13,6 +13,9 @@ cat assembly_summary_genbank.txt | cut -f 8,9,14,15,16,20 | grep metagenome > me
 #3. extract ftp links
 cat metagenomes_tbl.tsv | cut -f6 > ftp_links.txt
 
+# for .gbff you can just substitute
+# e.g.  sed 's/GCA/GCF/g' ftp_addresses.txt > gbff_adresses.txt
+
 #set up a shell script for downloading (DO IT IN SCREEN!)
 awk 'BEGIN{FS=OFS="/";filesuffix="genomic.gff.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print "rsync -t -v "ftpdir,file" ./"}' ftp_links.txt | sed 's/https/rsync/g' > download_gff_files.sh
 
